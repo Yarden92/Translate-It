0337b56c677b9565072761c3308fe1b350eeb93b
Yarden Cohen <yarden.c@hyro.ai>
2025-07-15 15:11:57 +0300

    Update UI components #4

--- temp_commit_file.js
+++ temp_commit_file.js
@@ -1,3 +1,4 @@
 // Random commit 1 - Tue Jul 15 15:11:57 IDT 2025
 // Random commit 2 - Tue Jul 15 15:11:57 IDT 2025
 // Random commit 3 - Tue Jul 15 15:11:57 IDT 2025
+// Random commit 4 - Tue Jul 15 15:11:57 IDT 2025

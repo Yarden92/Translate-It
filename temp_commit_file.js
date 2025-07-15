b7464467aaf287c7e2efeaf4bdf2abd231f69d59
Yarden Cohen <yarden.c@hyro.ai>
2025-07-15 15:11:57 +0300

    Improve accessibility #6

--- temp_commit_file.js
+++ temp_commit_file.js
@@ -3,3 +3,4 @@
 // Random commit 3 - Tue Jul 15 15:11:57 IDT 2025
 // Random commit 4 - Tue Jul 15 15:11:57 IDT 2025
 // Random commit 5 - Tue Jul 15 15:11:57 IDT 2025
+// Random commit 6 - Tue Jul 15 15:11:57 IDT 2025
